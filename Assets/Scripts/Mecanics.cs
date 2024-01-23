d7078c2f1b0ba64f023937d27938afe3d938b430
Alelith <eacaymo@gmail.com>
2024-01-23 12:13:20 +0100

    Modify mecanicToGuapaVariable

--- Assets/Scripts/Mecanics.cs
+++ Assets/Scripts/Mecanics.cs
@@ -8,7 +8,7 @@ public class Mecanics : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        mecanicToGuapa = 100;
     }
 
     // Update is called once per frame
